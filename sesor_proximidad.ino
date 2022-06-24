5113e22e536d1822c0bd3d00392761e5d5e7e9e2
davogarsa10 <josgarcia@uv.mx>
2022-06-24 08:15:15 -0500

    Update sesor_proximidad.ino
    actualización del programa

--- sesor_proximidad.ino
+++ sesor_proximidad.ino
@@ -1,36 +1,54 @@
 
+/*
+ * Proma de sensores de presencia para la ventana y establecer si esta completamente abierta o completamente cerrada
+ * Por: J. David Garcia Sarmiento
+ * Fecha: 24 de junio 2022
+ * Esto programa es parte del programa principal del proyecto capstone de Monitoreo de Ventilacion
+ */
 
-const int LED=13;
-const int S1=6;
-const int S2=2;
-int val1;
-int val2;
+ // Bibliotecas
 
-void setup(){
-
-  Serial.begin (9600);
+// Constantes 
+const int LED=13;            //Led de prueba 
+const int S1=6;              //Sensor de presencia 1 indicador de movimento de la ventana, y si esta abierto o cerrada la ventana
+const int S2=2;               //Sensor de presencia 1 indicador de movimento de la ventana, y si esta abierto o cerrada la ventana
   
-  pinMode(LED,OUTPUT);
-  pinMode(S1,INPUT);
-  pinMode(S2, INPUT);
-}
+// Variables
+int val1;                   //Variable de dato digital del sensor1 (1 o 0)
+int val2;                  //Variable de dato digital del sensor1 (1 o 0)
 
-void loop(){
-  val1=digitalRead(S1);
-  val2=digitalRead(S2);
+// Definición de objetos
 
-  Serial.print(val1);
-  Serial.print(val2);
+
+// Condiciones iniciales - Se ejecuta sólo una vez al energizar
+void setup(){              // Inicio de void setup ()
+   
+  Serial.begin (9600);    //comunicacion con el monitor serial
   
-  if  (!val1 && !val2){ // S1 y S2 = 1 led se apaga y se indica que la ventana esta cerrada
-    digitalWrite(LED,LOW);
-    Serial.println();
-    Serial.print("COMPLETAMENTE CERRADO");
-    }
+  pinMode(LED,OUTPUT);    //Parametros iniciales, establece el LED como salida
+  pinMode(S1,INPUT);      //Indicador de entrada de dato sensor1
+  pinMode(S2, INPUT);     //Indicador de entrada de dato sensor2
+}// Fin de void setup
+
+// Cuerpo del programa - Se ejecuta constamente
+void loop(){              // Inicio de void loop
+  val1=digitalRead(S1);   //Almacena el dato registrado por el sensor1
+  val2=digitalRead(S2);   //Almacena el dato registrado por el sensor2
+
+  Serial.print(val1);     //Muestra el valor en el monitor serial almanecado en la variable val1
+  Serial.print(val2);     //Muestra el valor en el monitor serial almanecado en la variable val2
+  
+  if  (!val1 && !val2){   // S1 y S2 = 1 led se apaga y se indica que la ventana esta cerrada
+    digitalWrite(LED,LOW);  //Manda la orden al LED que se apague
+    Serial.println();       //Salto de linea
+    Serial.print("COMPLETAMENTE CERRADO");    //Muestra este mensaje en el monitor serial 
+    }   
     
      else {               // S1 y S2 diferente de 1 led se prende y se indica que la ventana esta abierta
-      digitalWrite(LED,HIGH);
-      Serial.println();
-      Serial.print("COMPLETAMENTE ABIERTO");
+      digitalWrite(LED,HIGH);     //Manda la orden al LED que se apague
+      Serial.println();           //Salto de linea
+      Serial.print("COMPLETAMENTE ABIERTO"); //Muestra este mensaje en el monitor serial 
      }
-}
+}// Fin de void loop
+
+// Funcioes del usuario
