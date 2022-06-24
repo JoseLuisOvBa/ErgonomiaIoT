e28721f3ecc49a73bf37a09dc03259ed0b8a515a
JoseLuisOB <96042700+JoseLuisOB@users.noreply.github.com>
2022-06-24 09:53:02 -0500

    Nomenclatura de pines ESP32-CAM

--- Monitoreo_Ventilacion/Monitoreo_Ventilacion.ino
+++ Monitoreo_Ventilacion/Monitoreo_Ventilacion.ino
@@ -6,13 +6,21 @@
  * 
  * Este programa lee los sensores MQ135 (CO2) y Pir Hcsr501 (presencia) por MQTT
  * ritmo cardiaco (HR) y oxigenación (SpO2) por MQTT.
- *            GAS                        PRESENCIA                    
+ *            CO2                       PRESENCIA                    
  * ESP32      MQ135           ESP32      Pir Hcsr501      ESP32      LedInter  ESP32      Rele ventilador
  * I012 ------ 1 Datos        GND ------ 1 GND            I033 -----1 Datos    I013 ------1 Datos    
  * GND ------- 3 GND          I014 ------2 Datos                               VCC -------2 Vin  
  * VCC ------- 4 Vin          VCC -------3 Vin                                 GND ------ 3 GND                                                          
  * 
- *IO
+ **************IOs************
+ *04 
+ *02  PisV        Piston de Ventana
+ *14  Sv1         Sensor 1 de Ventana
+ *15  Sv2         Sensor 2 de Ventana
+ *13  Venti       Rele  Ventilador
+ *12  CO2         Sensor
+ *16  PIR         Sensor de Presencia
+ *33  Indicador de presencia Led Interno reflejado en el Dashboard
  *
  * Esto es una muestra de la estructura básica de un programa
  */
@@ -23,8 +31,8 @@
 
 
 // Constantes*************************************************************************
-float sensorValue; //CO2Value; variable para guardar el valor analógico del sensor
-int relayPin=13;    //RelayFan
+float CO2Value; //CO2Value; variable para guardar el valor analógico del sensor
+int Venti=13;    //Rele Ventilador
 int LedIn = 33;        //LedInterno
 int ValorPIR=0;
 
@@ -34,15 +42,15 @@ int data = 0; // Contador
 int wait = 5000;  // Indica la espera cada 5 segundos para envío de mensajes MQTT
 
 // Definición de objetos**************************************************************
-  #define CO2PIN 12
-  #define PIR 14
+  #define CO2 12
+  #define PIR 16
 
 // Condiciones iniciales - Se ejecuta sólo una vez al energizar***********************
 void setup() {
 
      pinMode(PIR, INPUT);
      pinMode(LedIn , OUTPUT);
-     pinMode (relayPin, OUTPUT);
+     pinMode (Venti, OUTPUT);
      
      Serial.begin (115200);
      Serial.println("El sensor de gas se esta pre-calentando");
@@ -61,8 +69,8 @@ void loop(){     //VOID LOOP****************************************************
       if (timeNow - timeLast > wait) { // Manda un mensaje por MQTT cada cinco segundos
       timeLast = timeNow; // Actualización de seguimiento de tiempo
 
-      presencia();                          //funcion de presencia  
-      gases();                              //Funcion deteccion de CO2
+      presencia();                          //Funcion de presencia  
+      COdos();                              //Funcion deteccion de CO2
     }// fin del if (timeNow - timeLast > wait)
 
 }// Fin de void loop*****************************************************************///////////////////
@@ -86,21 +94,21 @@ if (ValorPIR == HIGH){           //Pregunta si esta en alta
 
 
 
-//----------------------- GASES ---------------------------
+//----------------------- CO2 ---------------------------
 
-  void gases(){                //Esta funcion realiza el sensado de presencia
-  sensorValue = analogRead(CO2PIN); // lectura de la entrada analogica "A0""
+  void COdos(){                //Esta funcion realiza el sensado de presencia
+  CO2Value = analogRead(CO2); // lectura de la entrada analogica "A0""
   Serial.print("Valor detectado por el sensor: ");
-  Serial.print(sensorValue);
-  if(sensorValue > 600)   // La OMS sugiere de 400 a 600
+  Serial.print(CO2Value );
+  if(CO2Value  > 600)   // La OMS sugiere de 400 a 600
     {
-     Serial.print("  ¡Se ha detectado gas!  ");
-     digitalWrite (relayPin, HIGH);
+     Serial.print("  ¡Se ha detectado CO2!  ");
+     digitalWrite (Venti, HIGH);
      delay(2000);
     }
   else{
     Serial.print("");
-    digitalWrite (relayPin, LOW);
+    digitalWrite (Venti, LOW);
     delay(2000);
     }
   Serial.println("");
